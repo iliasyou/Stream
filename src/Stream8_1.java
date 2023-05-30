74c91c85c8bab56da8574f64fafd3ee1a11a93f2
ilias <ilias.marzak@gmail.com>
2023-05-30 14:42:47 +0200

    Commit and Push

--- src/Stream8_1.java
+++ src/Stream8_1.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StreamOefening7 {
+public class Stream8_1 {
     public static void main(String[] args) {
         List<String> Cities = new ArrayList<>();
         Cities.add(0, "Brussel");
@@ -15,20 +15,24 @@ public class StreamOefening7 {
                 .filter(c -> c.length() > 3)
                 .sorted(Comparator.reverseOrder()).map(String::toUpperCase)
                 .map(c -> c.substring(0, Math.min(c.length(), 10)))
+                .map(c->"prefix"+c+"suffix")
                 .collect(Collectors.joining(","));
+
+
 
         System.out.println(result);
 
 
     }
+
 }
-
-
 //Schrijf een methode die een lijst van Strings als parameter verwacht
-// en een lijst van Strings teruggeeft waarbij alle Strings die beginnen met een ‘a’
-// zijn en de lengte van de String groter is dan 3. de String.
-//De Strings moeten ook in omgekeerde volgorde
-// zijn en de Strings moeten worden samengevoegd tot een String met een komma als scheidingsteken.
-// De String moet ook in hoofdletters zijn.
+// en een lijst van Strings teruggeeft waarbij alle Strings die beginnen met een ‘a’zijn
+// en de lengte van de String groter is dan 3. de String.
+//De Strings moeten ook in omgekeerde volgorde zijn
+// en de Strings moeten worden samengevoegd tot een String met een komma als scheidingsteken.
+// De String moet ook in hoofdletter zijn.
 // De String moet ook worden afgekort tot 10 karakters.
+// De String moet ook worden voorzien van een prefix en een suffix.
 //public String filterA(List strings) { return null; }
+
---
+++ src/StreamOefeningen10_1.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class StreamOefeningen10_1 {
+    public static void main(String[] args) {
+        List<String> Cities = new ArrayList<>();
+        Cities.add(0, "Brussel");
+        Cities.add(1, "Antwerpen");
+        Cities.add(2, "Gent");
+        Cities.add(3, "Limburg");
+        Cities.add(4,"Aalst");
+        System.out.println("Cities from belgium");
+        System.out.println(filterA(Cities));
+
+
+
+    }
+
+    public static String filterA(List<String> strings) {
+        AtomicInteger counter = new AtomicInteger(0);
+        return strings.stream()
+
+                .filter(s -> s.startsWith("A"))
+                .filter(s -> s.length() > 3)
+                .sorted(Comparator.reverseOrder()).map(String::toUpperCase)
+                .map(s -> s.substring(0, Math.min(s.length(), 10)))
+                .map(s -> "prefix" + s + "suffix" +  counter.getAndIncrement())
+                .map(s-> s +"_"+ new Random().nextInt(100   ))
+                .collect(Collectors.joining(","));
+
+
+
+
+    }
+
+}
+//Schrijf een methode die een lijst van Strings als parameter verwacht
+// en een lijst van Strings teruggeeft waarbij alle Strings die beginnen met een ‘a’ zijn
+// en de lengte van de String groter is dan 3. de String.
+//De Strings moeten ook in omgekeerde volgorde zijn
+// en de Strings moeten worden samengevoegd tot een String met een komma als scheidingsteken.
+// De String moet ook in hoofdletter zijn. De String moet ook worden afgekort tot 10 karakters.
+// De String moet ook worden voorzien van een prefix en een suffix.
+// De String moet ook worden voorzien van een uniek nummer.
+// De String moet ook worden voorzien van een random getal.
+//public String filterA(List strings) { return null; }
